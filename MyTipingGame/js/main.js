badb14ff8f3f8f8aab915d25eb1ecd0a9a0286ca
otyahanaco <cyanos1001@gmail.com>
2020-04-23 17:04:51 +0900

    'ユーザーが入力した値が正しかった場合、表示テキストが＿になるよう追加'

--- MyTipingGame/js/main.js
+++ MyTipingGame/js/main.js
@@ -11,10 +11,19 @@
   const missLabel = document.getElementById('miss');  //missLabelにmissを取得する
   target.textContent = word;                        //targetのテキストコンテントにwordを設定
 
+  function updateTarget(){                   //updateTargetを定義する
+    let placeholder = '';                    //placeholderという変数を定義する。初期値は空
+    for (let i = 0; i < loc; i++) {             //iはloc番目まで１づつ増やす 初期値は0
+      placeholder += '_';                    //placeholder に_を追加していく
+    }
+    target.textContent = placeholder + word.substring(loc);//targetのテキストをsubstringのloc番目の文字列までpaceholderで上書きする
+  }
+
   window.addEventListener('keydown', (e) => { //キーが押されたときの処理
    console.log(e.key);                        //押し込まれたキーの値を取得する
    if (e.key === word[loc]) {                 //もし、e.keyの値とwordのloc番目の値が等しいとき
      loc++;                                   //locを一ずつ増やす
+     updateTarget();
      score++;                                 //scoreを1づつ増やす
      scoreLabel.textContent =  score;         //scoreLabelのテキストに 更新されたscoreを代入する
    } else {                                   //そうでない場合
@@ -23,3 +32,6 @@
    }
   });
 }
+
+
+//substring   部分文字列を取得するメソッド
