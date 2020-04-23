6f66d569ebdb35f0581155c68361ab2fe21bf1ea
otyahanaco <cyanos1001@gmail.com>
2020-04-23 20:30:16 +0900

    'アラートにスコアを表示させるよう変更'

--- MyTipingGame/js/main.js
+++ MyTipingGame/js/main.js
@@ -7,12 +7,12 @@
     "blue",
     "middle",
     "set",
-  ]
-  let word
+  ];
+  let word;
   let loc;            //locで今何文字目を売っているかをカウントする。初期値として0を代入
   let score;             //scoreという変数を定義する。初期値は０
   let miss;              //missという変数を定義する。初期値は0
-  const timeLimit = 6 * 3 * 10000;//timeLimit という定数を定義する 6*3*10000(3分)で初期化する
+  const timeLimit = 6 * 1 * 10000;//timeLimit という定数を定義する 6*1*10000(1分)で初期化する
   let startTime;             //startTimeという変数を定義する
   let isPlaying = false;     //isPlayingという変数をで意義　初期値をfalseとする
 
@@ -22,7 +22,7 @@
   const timerLabel = document.getElementById('timer');//timerLabelにtimerを取得する
 
 
-  function updateTarget(){                   //updateTargetを定義する
+  function updateTarget() {                   //updateTargetを定義する
     let placeholder = '';                    //placeholderという変数を定義する。初期値は空
     for (let i = 0; i < loc; i++) {          //iはloc番目まで１づつ増やす 初期値は0
       placeholder += '_';                    //placeholder に_を追加していく
@@ -40,18 +40,22 @@
 
     if (timeLeft < 0) {                    //もし、timeleftが0より小さい場合
       isPlaying = false;                   //isPlayingをfalseとする
-      clearTimeout(timeoutId);             //timeoutIdをリセットする
-      timerLabel.textContent = "0.00"      //timerLabelのテキストを　0.00とする
-      setTimeout (() => {                  //100ミリ秒後に
-        alert('GameOver');                 //アラートをGameOverと表示する
-      }, 100);
 
+      clearTimeout(timeoutId);             //timeoutIdをリセットする
+      timerLabel.textContent = "0.00";     //timerLabelのテキストを　0.00とする
+      setTimeout (() => {                  //100ミリ秒後に
+        shoeResult();                      //shoeResultを定義
+      }, 100);
       target.textContent = "click to replay";//targetのテキストを初期値にする
     }
   }
+  function shoeResult() {
+     const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
+     alert(`${score} letters, ${miss} misses ${accuracy.toFixed(2)}% accutacy!`);
+   }
   window.addEventListener('click', () => {   //クリックされたとき、
     if (isPlaying === true) {                //もしisPlayingがtrueの場合、これらの処理をしない
-      return
+      return;
     }
     isPlaying = true;                        //isPlaying をtrueとする
 
@@ -60,8 +64,9 @@
     miss = 0;                                //missの値を0にする
     scoreLabel.textContent = score;          //scoreLabelのテキストを初期値にする
     missLabel.textContent = miss;            //missLabelのテキストを初期値にする
-    target.textContent = word;               //targetのテキストをwordにする
     word = words[Math.floor(Math.random() * words.length)];
+
+    target.textContent = word;               //targetのテキストをwordにする
     startTime = Date.now();                  //startTimeを現在時刻とする
     updateTimer();                           //updateTimerという残り時間を計算する関数を定義する
   });
@@ -69,14 +74,14 @@
 
   window.addEventListener('keydown', (e) => { //キーが押されたときの処理
     if (isPlaying !== true) {                 //もしisPlayingが trueでない場合、これらの処理をしない
-      return
+      return;
     }
-   console.log(e.key);                        //押し込まれたキーの値を取得する
+
    if (e.key === word[loc]) {                 //もし、e.keyの値とwordのloc番目の値が等しいとき
      loc++;                                   //locを一ずつ増やす
      if (loc === word.length) {               //もし、locがwordの文字数と同じ数値になったとき
       word = words[Math.floor(Math.random() * words.length)]; //word=wordsのランダムな値とする
-      loc = 0                                                 //locを０とする
+      loc = 0;                                                //locを０とする
      }
      updateTarget();                          //uodateTrgetの処理を実行する
      score++;                                 //scoreを1づつ増やす
@@ -95,3 +100,6 @@
 //setTimeout ある処理を一定時間後に実行する
 //clearTimeout  時間処理をクリアする
 //alert  指定した要素をアラートとして表示させる　※仕様上重いので必要な処理を先に済ませて表示させるようにする。不具合の原因となる。
+
+//【不明点】
+//shoeResultの役割
