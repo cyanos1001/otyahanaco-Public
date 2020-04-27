886eda98dce95626eb3e1d66b0ba967245dd815c
otyahanaco <cyanos1001@gmail.com>
2020-04-27 22:01:51 +0900

    'ユーザーがliをクリックしたとき、mainに表示されるよう追加'

--- MySlideshow/js/main.js
+++ MySlideshow/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 {
-  const images = [
+  const images = [   //imagesという定数を定義
     'img/pic00.png',
     'img/pic01.png',
     'img/pic02.png',
@@ -11,17 +11,24 @@
     'img/pic06.png',
     'img/pic07.png',
   ];
-  let currentIndex = 0;
+  let currentIndex = 0;//currentIndexという変数を定義
 
-  const mainImage = document.getElementById('main')
-  mainImage.src = images[currentIndex];
+  const mainImage = document.getElementById('main');//mainを、mainImage(定数)として取得
+  mainImage.src = images[currentIndex];             //mainImageをimagesのcurrentIndex番目の要素とする
 
-  images.forEach(image => {
-    const img = document.createElement('img');
-    img.src = image;
+  images.forEach((image, index) => { //imagesの範囲のindex番目のアイテムをimageとしこれらの処理を繰り返す
+    const img = document.createElement('img');      //imgという定数にimgを与えて生成する
+    img.src = image;                                //imgのソースをimageとする
     
-    const li = document.createElement('li');
-    li.appendChild(img);
-    document.querySelector('.thumbnails').appendChild(li);
+    const li = document.createElement('li');        //liという定数をliに与えて生成する
+    if (index === currentIndex) {                   //indexがcuttentindexの値と同じだったとき
+      li.classList.add('current');                  //liにcurrentクラスを追加
+    }
+    li.addEventListener('click', () => {
+      mainImage.src = image;
+    });
+
+    li.appendChild(img);                            //liの子要素としてimgを追加する
+    document.querySelector('.thumbnails').appendChild(li);// thumbnailsの子要素としてliを追加する
   });
 }
